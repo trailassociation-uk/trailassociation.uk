31d17cf0386b26c22129492ca5ecb72e1bbb9b02
Alex Ward <alxwrd@googlemail.com>
2026-02-14 08:04:30 +0000

    fix colours

--- app/components/app/header.vue
+++ app/components/app/header.vue
@@ -11,7 +11,7 @@ import {
 
 
 <template>
-  <header class="fixed top-0 left-0 right-0 z-50 border-b border-border bg-background backdrop-blur supports-[backdrop-filter]:bg-background/80">
+  <header class="fixed top-0 left-0 right-0 z-50 border-b border-foreground bg-background backdrop-blur">
     <div class="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
       <NuxtLink to="/" class="flex items-center gap-2 font-semibold">
         <MapPin class="size-6 text-red-600" aria-hidden="true" />
