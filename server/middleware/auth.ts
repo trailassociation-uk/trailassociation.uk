d48a669354105dc22d659eaa93487e14cf1bd1ce
Alex Ward <alxwrd@googlemail.com>
2026-06-09 20:37:48 +0100

    also skip auth validation for prerendered routes

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,4 +1,8 @@
 export default defineEventHandler(async (event) => {
+  if (import.meta.prerender) {
+    event.context.auth = { user: null };
+    return;
+  }
   const session = await getUserSession(event);
   event.context.auth = { user: session?.user ?? null };
 });
